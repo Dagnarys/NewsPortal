6e8d4a208f33dcaa849ee4b45d954aaabb1fb4e5
dagnarys <super.iard@yandex.ru>
2025-03-06 22:39:49 +0300

    update news screen

---
+++ news_portal/lib/screens/screen_news_main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:news_portal/components/category_bar.dart';
+import 'package:news_portal/components/nav_bar.dart';
+import 'package:news_portal/components/top_bar.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          //верхняя панель
+          const TopBar(),
+          //пространство между верхней панелью и панелью с категориями
+          
+          SizedBox(height: 5,),
+          // панель с категорями 
+          CategoryBar(),
+          //пространство для заполнения карточек с новостью
+          Expanded(child: Container()),
+          //нижняя панель навигации
+          const NavBar(),
+
+        ],
+      ),
+    );
+  }
+}
