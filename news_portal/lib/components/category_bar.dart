3edc80d8d423c8fddd2b68e2792ff7d6fe480d1c
dagnarys <super.iard@yandex.ru>
2025-03-06 22:36:57 +0300

    added category_bar

---
+++ news_portal/lib/components/category_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CategoryBar extends StatelessWidget {
+  const CategoryBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Row(
+        children: [ 
+          //пару чипсов, переход фильтрация по категории
+          
+
+
+          //кнопка перехода на screen_category
+
+
+      ],),
+    );
+  }
+}
