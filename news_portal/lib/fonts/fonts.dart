a613c83ea175dea5d2c68872f71d6d09e2f1276d
dagnarys <super.iard@yandex.ru>
2025-03-08 23:24:48 +0300

    added font nunito

---
+++ news_portal/lib/fonts/fonts.dart
@@ -0,0 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
+
+// Статическая переменная для получения fontFamily
+class AppFonts {
+  static String get nunitoFontFamily => GoogleFonts.nunito().fontFamily!;
+}
