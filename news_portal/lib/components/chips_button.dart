bde65ad57acdc9f641b40140df404be51e86f8a5
dagnarys <super.iard@yandex.ru>
2025-03-11 16:53:27 +0300

    added heigth to chips

--- news_portal/lib/components/chips_button.dart
+++ news_portal/lib/components/chips_button.dart
@@ -9,27 +9,31 @@ class ChipsButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
-        
-      },
-      style: TextButton.styleFrom(
-        padding: EdgeInsets.zero, // Убираем стандартный padding
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-          side: const BorderSide(color: AppColors.borderColor),
+    return Container(
+      height: 26,
+      padding: const EdgeInsets.fromLTRB(2, 0, 2, 0),
+      child: TextButton(
+        onPressed: () {
+          
+        },
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(0), // Убираем стандартный padding
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+            side: const BorderSide(color: AppColors.borderColor),
+          ),
         ),
-      ),
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(3, 1, 3, 2),
-        child: Text(
-          '#студентам',
-          style: TextStyle(
-            fontFamily: AppFonts.nunitoFontFamily,
-            color: Colors.black,
-            decoration: TextDecoration.none,
-            fontSize: 12,
-            fontWeight: FontWeight.w600,
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(3, 1, 3, 2),
+          child: Text(
+            '#студентам',
+            style: TextStyle(
+              fontFamily: AppFonts.nunitoFontFamily,
+              color: Colors.black,
+              decoration: TextDecoration.none,
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ),
       ),
