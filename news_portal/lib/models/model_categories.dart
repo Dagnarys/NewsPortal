e8365d658154f7ada6a7f01f4c26053b6adf07e5
dagnarys <super.iard@yandex.ru>
2025-03-12 16:05:48 +0300

    Create model_categories.dart

---
+++ news_portal/lib/models/model_categories.dart
@@ -0,0 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// models/model_categories.dart
+class Category {
+  final String name;
+
+  Category({required this.name});
+
+  factory Category.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Category(
+      name: data['name'] ?? 'Unknown',
+    );
+  }
+}
