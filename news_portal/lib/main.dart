78b85c20c374a6eaceb0d46e850e7e914dbb2d55
dagnarys <super.iard@yandex.ru>
2025-03-12 16:05:43 +0300

    added async for firebase realtime

--- news_portal/lib/main.dart
+++ news_portal/lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:news_portal/firebase_options.dart';
 import 'package:news_portal/screens/screen_news_main.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+    );
+    print('Firebase initialized successfully');
+  } catch (e) {
+    print('Error initializing Firebase: $e');
+  }
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
