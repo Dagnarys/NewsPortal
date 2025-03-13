780c32882ac8acc5cbb1bfc44b8ad3552129d470
dagnarys <super.iard@yandex.ru>
2025-03-13 23:48:30 +0300

    added fontSize required

--- news_portal/lib/components/chips_button.dart
+++ news_portal/lib/components/chips_button.dart
@@ -7,35 +7,37 @@ class ChipsButton extends StatelessWidget {
 
   final String label;
   final VoidCallback? onPressed;
+  final double fontSize;
 
-  const ChipsButton({super.key, required this.label, this.onPressed});
+  const ChipsButton({super.key, required this.label, this.onPressed, required this.fontSize});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 26,
+      margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
       padding: const EdgeInsets.fromLTRB(2, 0, 2, 0),
       child: TextButton(
         onPressed: onPressed ??() {
         },
         style: TextButton.styleFrom(
           minimumSize: Size(double.minPositive,double.minPositive),
-          padding: const EdgeInsets.all(0), // Убираем стандартный padding
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0), // Убираем стандартный padding
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
             side: const BorderSide(color: AppColors.borderColor),
           ),
         ),
         child: Container(
-          padding: const EdgeInsets.fromLTRB(3, 1, 3, 2),
+          padding: const EdgeInsets.fromLTRB(4, 1, 5, 0),
           child: Text(
             '#$label',
             style: TextStyle(
+              fontSize: fontSize,
               fontFamily: AppFonts.nunitoFontFamily,
-              color: Colors.black,
+              color: Color(0xFF2E0505),
               decoration: TextDecoration.none,
-              fontSize: 12,
-              fontWeight: FontWeight.w600,
+              fontWeight: FontWeight.w800,
             ),
           ),
         ),
