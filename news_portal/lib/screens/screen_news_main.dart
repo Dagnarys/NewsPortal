406024304b939cdbde33befaa87ea2580ce0f911
dagnarys <super.iard@yandex.ru>
2025-03-14 23:18:58 +0300

    added stack and list vuew w/ cards

--- news_portal/lib/screens/screen_news_main.dart
+++ news_portal/lib/screens/screen_news_main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_portal/components/category_bar.dart';
 import 'package:news_portal/components/nav_bar.dart';
+import 'package:news_portal/components/news_card.dart';
 import 'package:news_portal/components/top_bar.dart';
 
 class MainScreen extends StatelessWidget {
@@ -8,27 +9,48 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:  Container(
-        color: Colors.white,
-        child: Column(
-          children: [
-            //верхняя панель
-            const TopBar(),
-            //пространство между верхней панелью и панелью с категориями
-      
-            const SizedBox(height: 5,),
-            // панель с категорями 
-            const CategoryBar(),
-            
-            //пространство для заполнения карточек с новостью
-            Expanded(child: Container()),
-            //нижняя панель навигации
-            const NavBar(),
-      
-          ],
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          color: Colors.white,
+          child: Column(
+            children: [
+              //верхняя панель
+              const TopBar(),
+              //пространство между верхней панелью и панелью с категориями
+
+              const SizedBox(
+                height: 5,
+              ),
+              // панель с категорями
+              const CategoryBar(),
+
+              const SizedBox(
+                height: 0,
+              ),
+              Expanded(
+                child: ListView(
+                  padding: EdgeInsets.all(0),
+                  children: [
+                    const SizedBox(
+                      height: 7,
+                    ),
+                    NewsCard(),
+                    NewsCard(),
+                    NewsCard(),
+                    NewsCard(),
+                    // Можно добавить больше NewsCard() при необходимости
+                  ],
+                ),
+              ),
+
+              //пространство для заполнения карточек с новостью
+              //нижняя панель навигации
+            ],
+          ),
         ),
-      ),
+        const NavBar(),
+      ]),
     );
   }
 }
