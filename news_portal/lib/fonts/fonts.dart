d9930e5812fe0b9617301f4d372271d17656a54c
dagnarys <super.iard@yandex.ru>
2025-03-14 23:18:32 +0300

    added nunito_sans

--- news_portal/lib/fonts/fonts.dart
+++ news_portal/lib/fonts/fonts.dart
@@ -3,4 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 // Статическая переменная для получения fontFamily
 class AppFonts {
   static String get nunitoFontFamily => GoogleFonts.nunito().fontFamily!;
+  static String get nunitoSansFontFamily => GoogleFonts.nunitoSans().fontFamily!;
+ 
 }
