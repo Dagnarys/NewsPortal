7b0582d53aa46d97f23ac71153ae5c3e426bb3cd
dagnarys <super.iard@yandex.ru>
2025-03-21 21:15:48 +0300

    checked lisview img

--- news_portal/lib/screens/screen_new.dart
+++ news_portal/lib/screens/screen_new.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:news_portal/components/top_bar.dart';
 import 'package:news_portal/models/model_news.dart';
 import 'package:news_portal/repositories/news.dart';
 
@@ -10,43 +10,105 @@ class ScreenNew extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Новость'),
-      ),
-      body: FutureBuilder<News>(
-        future: repositoryNews.getNewsById(newsId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Ошибка при загрузке новости: ${snapshot.error}'));
-          } else if (!snapshot.hasData) {
-            return Center(child: Text('Новость не найдена'));
-          } else {
-            News newsItem = snapshot.data!;
-            return SingleChildScrollView(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          TopBar(),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '25 октября 2024',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Инженер – это множественное число',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Автор текста: Марина Ткачева',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+              SizedBox(height: 16),
+              Row(
                 children: [
                   Text(
-                    newsItem.title,
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
+                    '#категория #категория',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
                   ),
-                  SizedBox(height: 16),
+                  Spacer(),
                   Text(
-                    newsItem.content,
-                    style: TextStyle(fontSize: 16),
+                    '© 1234',
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
                   ),
                 ],
               ),
-            );
-          }
-        },
+              SizedBox(height: 16),
+              Text(
+                'Дискуссия «Университеты: как раскрыть потенциал обучающихся в эпоху технологий» состоялась 25 октября на площадке СберУниверситета. Участники обсудили, как готовить выпускников, способных управлять технологиями на благо, что ещё критически важно в образе их создателя, как эффективнее реализовать новые смыслы в образовательных программах. В обсуждении принял участие проректор по науке и цифровому развитию МГТУ им. Н.Э. Баумана Павел Дроговоз. Модератором дискуссии выступил руководитель центра по работе с вузами и академическим сообществом СберУниверситета Дмитрий Коваленко.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Как известно, высшая школа играет важнейшую роль в формировании собственного Я, закладывает фундамент дальнейшего образования и постоянного стремления к саморазвитию, самосовершенствованию. В своём выступлении Павел Дроговоз говорил о том, как в Бауманском университете смотрят на будущее и воспитывают инженеров, которым и предстоит его создавать своими руками.',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              Text(
+                '«Для нас будущее уже наступило, — полушутя отметил проректор. — 2030 год для нас уже реален: мы набрали тех, кто выйдет в индустрию в тридцатом году».',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                    height: 150, // Задайте нужную высоту
+                    child: ListView(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        // Ваши элементы списка
+                        Container(
+                          width: 100,
+                          color: Colors.red,
+                        ),
+                        SizedBox(width: 10,),
+                        Container(
+                          width: 100,
+                          color: Colors.blue,
+                        ),
+                        SizedBox(width: 10,),
+                        Container(
+                          width: 100,
+                          color: Colors.green,
+                        ),
+                        SizedBox(width: 10,),
+                         // Ваши элементы списка
+                        Container(
+                          width: 100,
+                          color: Colors.red,
+                        ),
+                        SizedBox(width: 10,),
+                        Container(
+                          width: 100,
+                          color: Colors.blue,
+                        ),SizedBox(width: 10,),
+                        Container(
+                          width: 300,
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                    )],
+                    ),
+                  ),
+          ),
+        ],
       ),
     );
   }
