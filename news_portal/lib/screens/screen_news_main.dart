d5d36a3ae80a6370490e12db216bb071b7c75568
dagnarys <super.iard@yandex.ru>
2025-03-21 21:16:09 +0300

    remove unecessary pack

--- news_portal/lib/screens/screen_news_main.dart
+++ news_portal/lib/screens/screen_news_main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:news_portal/components/category_bar.dart';
 import 'package:news_portal/components/nav_bar.dart';
