4e94fc5cdd1f181839398a7b7c904b46069b6f32
dagnarys <super.iard@yandex.ru>
2025-03-24 11:36:01 +0300

    added id category

--- news_portal/lib/models/model_categories.dart
+++ news_portal/lib/models/model_categories.dart
@@ -2,13 +2,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 // models/model_categories.dart
 class Category {
+  final String id; // Добавляем ID категории
   final String name;
 
-  Category({required this.name});
+  Category({required this.id, required this.name});
 
   factory Category.fromFirestore(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
     return Category(
+      id: doc.id, // ID документа Firestore
       name: data['name'] ?? 'Unknown',
     );
   }
