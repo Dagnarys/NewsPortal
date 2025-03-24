6b3fcc49816c1815921e864cca62b9bb41b0de14
dagnarys <super.iard@yandex.ru>
2025-03-24 11:36:15 +0300

    added categoryId for FK

--- news_portal/lib/models/model_news.dart
+++ news_portal/lib/models/model_news.dart
@@ -1,23 +1,36 @@
-  import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:news_portal/repositories/categories.dart';
 
   // Модель для новости
-  class News {
-    final String id;
-    final String title;
-    final String content;
+class News {
+  final String id;
+  final String title;
+  final String content;
+  final String categoryId; // Связь с категорией
 
-    News({
-      required this.id,
-      required this.title,
-      required this.content,
-    });
+  News({
+    required this.id,
+    required this.title,
+    required this.content,
+    required this.categoryId, // Добавляем categoryId
+  });
 
-    factory News.fromFirestore(DocumentSnapshot doc) {
-      final data = doc.data() as Map<String, dynamic>;
-      return News(
-        id: doc.id,
-        title: data['title'] ?? 'No Title',
-        content: data['content'] ?? 'No Content',
-      );
+  factory News.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return News(
+      id: doc.id,
+      title: data['title'] ?? 'No Title',
+      content: data['content'] ?? 'No Content',
+      categoryId: data['id_category'] ?? '', // Получаем categoryId из Firestore
+    );
+  }
+  Future<String> getCategoryName(CategoriesRepository categoryRepo) async {
+    if (categoryId.isEmpty) return 'нет категории';
+    try {
+      final category = await categoryRepo.getCategory(categoryId);
+      return category?.name ?? 'Неизвестная категория';
+    } catch (e) {
+      return 'Неизвестная категория';
     }
   }
+}
