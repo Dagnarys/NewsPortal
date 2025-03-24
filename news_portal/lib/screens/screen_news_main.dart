6246df09e44ef294ac493e5502ba6716b0838edc
dagnarys <super.iard@yandex.ru>
2025-03-24 14:50:45 +0300

    remove news_stream to components

--- news_portal/lib/screens/screen_news_main.dart
+++ news_portal/lib/screens/screen_news_main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:news_portal/components/category_bar.dart';
+import 'package:news_portal/components/filter_button.dart';
 import 'package:news_portal/components/nav_bar.dart';
-import 'package:news_portal/components/news_card.dart';
+import 'package:news_portal/components/news_stream.dart';
 import 'package:news_portal/components/top_bar.dart';
-import 'package:news_portal/models/model_news.dart';
 import 'package:news_portal/repositories/news.dart';
+
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -13,7 +14,8 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final NewsRepository repositoryNews = NewsRepository();
     return Scaffold(
-      body: Stack(children: [
+      body: 
+      Stack(children: [
         Container(
           color: Colors.white,
           child: Column(
@@ -21,68 +23,27 @@ class MainScreen extends StatelessWidget {
               //верхняя панель
               const TopBar(),
               //пространство между верхней панелью и панелью с категориями
-
+    
               const SizedBox(
                 height: 5,
               ),
               // панель с категорями
               const CategoryBar(),
-
+    
               const SizedBox(
                 height: 0,
               ),
-               Expanded(child: NewsStream(repositoryNews: repositoryNews))
-
+               NewsStream(repositoryNews: repositoryNews)
+    
               //пространство для заполнения карточек с новостью
               //нижняя панель навигации
             ],
           ),
         ),
+        FilterButton(),
         const NavBar(),
       ]),
     );
   }
 }
 
-class NewsStream extends StatefulWidget {
-  const NewsStream({
-    super.key,
-    required NewsRepository repositoryNews,
-  }) : _repositoryNews = repositoryNews;
-
-  final NewsRepository _repositoryNews;
-
-  @override
-  State<NewsStream> createState() => _NewsStreamState();
-}
-
-class _NewsStreamState extends State<NewsStream> {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<List<News>>(
-        stream: widget._repositoryNews.getNewsStream(),
-        builder: (context, snapshot) {
-         
-          if (snapshot.hasData) {
- 
-            List<News> newsList = snapshot.data!;
-            print(newsList);
-            return ListView.builder(
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              itemCount:newsList.length,
-              itemBuilder:(context,index) {
-              //получаем каждый элемент
-                News newsItem = newsList[index];
-                return NewsCard(key: ValueKey(newsItem.id), title: newsItem.title, text: newsItem.content, newsId: newsItem.id,);
-                
-            },
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Ошибка при загрузке новостей: ${snapshot.error}'));
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        });
-  }
-}
