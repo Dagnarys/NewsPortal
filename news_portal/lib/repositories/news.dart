198b0be81f6a8946d010c37c5b92c3e2173d3c8f
dagnarys <super.iard@yandex.ru>
2025-03-24 17:18:56 +0300

    update newsStream

--- news_portal/lib/repositories/news.dart
+++ news_portal/lib/repositories/news.dart
@@ -4,11 +4,17 @@ import 'package:news_portal/models/model_news.dart';
 class NewsRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Stream<List<News>> getNewsStream() {
-    return _firestore.collection('news').snapshots().map((snapshot) {
+  // Метод для получения потока новостей с возможностью фильтрации по categoryId
+  Stream<List<News>> getNewsStream({String? categoryId}) {
+    Query<Map<String, dynamic>> query = _firestore.collection('news');
+    if (categoryId != null) {
+      query = query.where('id_category', isEqualTo: categoryId); // Фильтрация по категории
+    }
+    return query.snapshots().map((snapshot) {
       return snapshot.docs.map((doc) => News.fromFirestore(doc)).toList();
     });
   }
+  // Метод для получения новости по id
   Future<News> getNewsById(String id) async {
     final doc = await FirebaseFirestore.instance.collection('news').doc(id).get();
     if (doc.exists) {
@@ -17,5 +23,16 @@ class NewsRepository {
       throw Exception('Новость с ID $id не найдена');
     }
   }
-   
+  
+  // Метод для обновления данных
+  Future<void> refreshData() async {
+    try {
+      // Здесь можно добавить логику обновления данных
+      // Например, принудительно перезагрузить коллекцию из Firestore
+      await _firestore.collection('news').get();
+      print('Данные обновлены');
+    } catch (e) {
+      print('Ошибка при обновлении данных: $e');
+    }
+  }
 }
