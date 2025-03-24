a996380fd118246d01a98d8d781d6a705cb7a0b4
dagnarys <super.iard@yandex.ru>
2025-03-24 17:17:15 +0300

    reformat and added image url

--- news_portal/lib/components/news_card.dart
+++ news_portal/lib/components/news_card.dart
@@ -7,99 +7,114 @@ class NewsCard extends StatelessWidget {
   final String title;
   final String content;
   final String newsId;
-  final String name_category;
-  const NewsCard({super.key, required this.title, required this.content , required this.newsId,required this.name_category});
+  final String nameCategory;
+  final String imageUrl;
+  const NewsCard({
+    super.key,
+    required this.title,
+    required this.content,
+    required this.newsId,
+    required this.nameCategory,
+    required this.imageUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextButton(
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>  ScreenNew(newsId: newsId)));
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => ScreenNew(
+                        newsId: newsId,
+                        content: content,
+                        title: title,
+                      )));
         },
         child: Container(
           padding: EdgeInsets.fromLTRB(8, 55, 8, 8),
           width: 321,
           height: 205,
-          
           decoration: BoxDecoration(
-            // image:DecorationImage(image: NetworkImage('https://downloader.disk.yandex.ru/preview/190e43a026c3ea934a08bb1b5ab73a1b6a93d59b210ae191d677958a8d86203a/67d4af9d/Wyyw8bpFhq8nDv_t4lp8psXsfcWgY1eLAQ_TRC8Is9LtQgp9ua0x_mAaf3VHifgM6svxM_Fu1a4VrBAiz6LWiA%3D%3D?uid=0&filename=image_card.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=0&tknv=v2&size=2048x2048'),
-            // fit: BoxFit.cover,),
-            color: const Color.fromARGB(255, 6, 6, 6),
-            borderRadius: BorderRadius.all(Radius.circular(16))
-        
-          ),
+              color: imageUrl.isNotEmpty
+                  ? null
+                  : Colors.black, // Черный фон, если изображение отсутствует
+              image: imageUrl.isNotEmpty
+                  ? DecorationImage(
+                      image: NetworkImage(imageUrl), // Загрузка изображения
+                      fit: BoxFit.cover,
+                    )
+                  : null, // Если imageUrl пустой, изображение не используется
+              borderRadius: BorderRadius.all(Radius.circular(16))),
           child: Column(
             children: [
               Text(
                 title,
                 style: TextStyle(
-                  fontSize: 18,
-                  fontFamily: AppFonts.nunitoSansFontFamily,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white
-                ),
-                
+                    fontSize: 18,
+                    fontFamily: AppFonts.nunitoSansFontFamily,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
-              SizedBox(height: 26,),
+              SizedBox(
+                height: 26,
+              ),
               Text(
-                maxLines: 4,
-                overflow: TextOverflow.ellipsis,
-                content,
-                style: TextStyle(
-                fontSize: 12,
-                fontFamily: AppFonts.nunitoFontFamily,
-                color: Colors.white)
-                ),
+                  maxLines: 4,
+                  overflow: TextOverflow.ellipsis,
+                  content,
+                  style: TextStyle(
+                      fontSize: 12,
+                      fontFamily: AppFonts.nunitoFontFamily,
+                      color: Colors.white)),
               Expanded(child: Container()),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                  name_category,
-                  style: TextStyle(
-                  fontSize: 12,
-                  fontFamily: AppFonts.nunitoFontFamily,
-                  color: Colors.white
-                ),),
-                Row(
-                  children: [
-                     SvgPicture.asset(
-                      'assets/svg/comment.svg',
-                    ),
-                    SizedBox(width: 2,),
-                    Text(
-                      '1234',
-                       style: TextStyle(
-                        fontSize: 10,
+                    nameCategory,
+                    style: TextStyle(
+                        fontSize: 12,
                         fontFamily: AppFonts.nunitoFontFamily,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFFB3B3B3)
-                      )
-                    ) , 
-                    SizedBox(width: 4,),
-                    SvgPicture.asset(
-                      'assets/svg/eye.svg',
-                    ),
-                    SizedBox(width: 2,),
-                    Text(
-                      '1234',
-                       style: TextStyle(
-                      fontSize: 10,
-                      fontFamily: AppFonts.nunitoFontFamily,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFB3B3B3)
-                      )
-                    ) ,      
-                  ],
-                ),
-          
+                        color: Colors.white),
+                  ),
+                  Row(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/svg/comment.svg',
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Text('1234',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: AppFonts.nunitoFontFamily,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFB3B3B3))),
+                      SizedBox(
+                        width: 4,
+                      ),
+                      SvgPicture.asset(
+                        'assets/svg/eye.svg',
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Text('1234',
+                          style: TextStyle(
+                              fontSize: 10,
+                              fontFamily: AppFonts.nunitoFontFamily,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFFB3B3B3))),
+                    ],
+                  ),
                 ],
               )
             ],
           ),
-        
         ),
       ),
     );
