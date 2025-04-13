a8529bb56b1c90ed9fe89ad207ce7dcd193b3ddf
dagnarys <super.iard@yandex.ru>
2025-04-13 23:13:49 +0300

    Update news.dart

--- news_portal/lib/repositories/news.dart
+++ news_portal/lib/repositories/news.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:news_portal/models/model_news.dart';
+import 'package:news_portal/repositories/images.dart';
 
 class NewsRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final ImagesRepository _imagesRepo = ImagesRepository();
 
   // Метод для получения потока новостей с возможностью фильтрации по categoryId
   Stream<List<News>> getNewsStream({String? categoryId}) {
@@ -10,20 +12,33 @@ class NewsRepository {
     if (categoryId != null) {
       query = query.where('id_category', isEqualTo: categoryId); // Фильтрация по категории
     }
-    return query.snapshots().map((snapshot) {
-      return snapshot.docs.map((doc) => News.fromFirestore(doc)).toList();
+    return query.snapshots().asyncMap((snapshot) async {
+      final newsList = <News>[];
+      for (final doc in snapshot.docs) {
+        final news = News.fromFirestore(doc);
+        // Получаем URL изображения для каждой новости
+        final imageUrl = await _imagesRepo.getImageUrl(news.imageId);
+        news.imageUrl = imageUrl; // Добавляем URL в модель
+        newsList.add(news);
+      }
+      return newsList;
     });
   }
+
   // Метод для получения новости по id
   Future<News> getNewsById(String id) async {
-    final doc = await FirebaseFirestore.instance.collection('news').doc(id).get();
+    final doc = await _firestore.collection('news').doc(id).get();
     if (doc.exists) {
-      return News.fromFirestore(doc);
+      final news = News.fromFirestore(doc);
+      // Получаем URL изображения для новости
+      final imageUrl = await _imagesRepo.getImageUrl(news.imageId);
+      news.imageUrl = imageUrl; // Добавляем URL в модель
+      return news;
     } else {
       throw Exception('Новость с ID $id не найдена');
     }
   }
-  
+
   // Метод для обновления данных
   Future<void> refreshData() async {
     try {
