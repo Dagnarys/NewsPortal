4884dd1cb4dd1424ffb69fd9162f43a9df57bc27
dagnarys <super.iard@yandex.ru>
2025-04-21 10:48:57 +0300

    added loadCurrentUser to check state

--- news_portal/lib/providers/user_provider.dart
+++ news_portal/lib/providers/user_provider.dart
@@ -5,22 +5,42 @@ import 'package:news_portal/repositories/user.dart';
 import 'package:news_portal/screens/screen_news_main.dart';
 
 class UserProvider with ChangeNotifier {
-  String? _role;
 
+  final UserRepository _userRepository = UserRepository();
+  String? _role;
+  String? _userId;
   String? get role => _role;
 
   bool get isGuest => _role == null;
   bool get isUser => _role == 'user';
   bool get isModerator => _role == 'moderator';
 
+  String? get userId => _userId;
+
   void setRole(String newRole) {
     _role = newRole;
     notifyListeners();
   }
 
+  // Метод для установки userId (например, при входе в систему)
+  void setUserId(String id) {
+    _userId = id;
+    notifyListeners(); // Уведомляем слушателей об изменении состояния
+  }
   void clearRole() {
     _role = null;
     notifyListeners();
+  }
+    Future<void> loadCurrentUser() async {
+    try {
+      final userData = await _userRepository.getCurrentUserData();
+      if (userData != null) {
+        _userId = FirebaseAuth.instance.currentUser!.uid; // Устанавливаем userId
+        notifyListeners();
+      }
+    } catch (e) {
+      print('Ошибка загрузки данных пользователя: $e');
+    }
   }
   Future<void> signOut(BuildContext context) async {
     final userRepository = UserRepository(); // Инициализация репозитория
