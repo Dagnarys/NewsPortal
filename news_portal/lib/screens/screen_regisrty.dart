390039bf0e3102a99f9d8cb33407df390231fea3
dagnarys <super.iard@yandex.ru>
2025-04-21 10:46:57 +0300

    added name surname phone

--- news_portal/lib/screens/screen_regisrty.dart
+++ news_portal/lib/screens/screen_regisrty.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:news_portal/components/nav_bar.dart';
 import 'package:news_portal/components/top_bar.dart';
 import 'package:news_portal/const/colors.dart';
+import 'package:news_portal/fonts/fonts.dart';
 import 'package:news_portal/repositories/user.dart';
 import 'package:news_portal/screens/screen_auth.dart';
+import 'package:ru_phone_formatter/ru_phone_formatter.dart';
 
 class ScreenRegistry extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _surnameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
 
+  final maskFormatter = RuPhoneInputFormatter();
   final UserRepository _userRepository =
       UserRepository(); // Экземпляр репозитория
 
@@ -18,6 +24,9 @@ class ScreenRegistry extends StatelessWidget {
       await _userRepository.registerWithEmailAndPassword(
         _emailController.text.trim(),
         _passwordController.text.trim(),
+        _nameController.text.trim(),
+        _surnameController.text.trim(),
+        _phoneNumberController.text.trim(),
       );
 
       // Уведомляем пользователя об успешной регистрации
@@ -41,61 +50,147 @@ class ScreenRegistry extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TopBar(),
-          Expanded(child: Container()),
-          Container(
-            width: 353,
-            child: Column(
-              children: [
-                TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            width: 1,
-                            color: AppColors.primaryColor,
-                            style: BorderStyle.solid)),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            width: 1,
-                            color: AppColors.primaryColor,
-                            style: BorderStyle.solid)),
+      body: Stack(
+        children:[ Column(
+        
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            TopBar(),
+            Text('Регистрация', style: TextStyle(fontSize: 30, fontFamily: AppFonts.nunitoFontFamily, fontWeight: FontWeight.w600),),
+            Flexible( // Используем Flexible вместо Expanded
+            fit: FlexFit.loose,
+            child: SizedBox.shrink()),
+            Container(
+              width: 353,
+              child:Column(
+                children: [
+                  TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Имя',
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                    ),
                   ),
-                ),
-                SizedBox(height: 10,),
-                TextField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    labelText: 'Пароль',
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            width: 1,
-                            color: AppColors.primaryColor,
-                            style: BorderStyle.solid)),
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                            width: 1,
-                            color: AppColors.primaryColor,
-                            style: BorderStyle.solid)),
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => _register(context),
-                  child: Text('Зарегистрироваться'),
-                ),
-              ],
+                  TextField(
+                    controller: _surnameController,
+                    decoration: InputDecoration(
+                      labelText: 'Фамилия',
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    inputFormatters: [maskFormatter],
+                    controller: _phoneNumberController,
+                    decoration: InputDecoration(
+                      labelText: 'Номер телефона',
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Пароль',
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                          borderSide: BorderSide(
+                              width: 1,
+                              color: AppColors.primaryColor,
+                              style: BorderStyle.solid)),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextButton(
+                      onPressed: () => _register(context),
+                      child: Container(
+                        width: 254,
+                        height: 56,
+                        decoration: BoxDecoration(
+                            gradient: AppColors.buttonGradient,
+                            borderRadius: BorderRadius.all(Radius.circular(10))),
+                        child: Center(
+                            child: Text(
+                          'Зарегистрироваться',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16),
+                        )),
+                      )),
+                ],
+              ),
             ),
-          ),
-          Expanded(child: Container()),
-          NavBar(),
-        ],
-      ),
+
+          ],
+        ),
+        Align( // Позиционируем NavBar внизу экрана
+          alignment: Alignment.bottomCenter,
+          child: NavBar())]),
     );
   }
 }
