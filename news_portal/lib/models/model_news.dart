bc7af2b950692f571eef986b2675ed5c1aa1c830
dagnarys <super.iard@yandex.ru>
2025-05-09 14:29:08 +0300

    Update model_news.dart

--- news_portal/lib/models/model_news.dart
+++ news_portal/lib/models/model_news.dart
@@ -1,37 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:news_portal/repositories/categories.dart';
-import 'package:news_portal/repositories/images.dart';
 
-  // Модель для новости
+
 class News {
   final String id;
   final String title;
   final String content;
-  final String categoryId; // Связь с категорией
-  final String imageId; // ID изображения из коллекции images
-  String imageUrl; // Поле для хранения URL изображения
+  final String categoryId;
+  final List<dynamic> images; // просто пути или ID изображений
 
   News({
     required this.id,
     required this.title,
     required this.content,
     required this.categoryId,
-    required this.imageId,
-    this.imageUrl = '', // Инициализируем пустой строкой по умолчанию
+    required this.images,
   });
 
   factory News.fromFirestore(DocumentSnapshot doc) {
     final data = doc.data() as Map<String, dynamic>;
+    final imageList = (data['images'] as List?)?.map((item) => item.toString()).toList() ?? [];
+
     return News(
       id: doc.id,
-      title: data['title'] ?? 'No Title',
-      content: data['content'] ?? 'No Content',
-      categoryId: data['id_category'] ?? '', // Получаем categoryId из Firestore
-      imageId: data['image_url'] ?? '', // Получаем imageId из Firestore
+      title: data['title'] ?? 'Без заголовка',
+      content: data['content'] ?? 'Нет текста',
+      categoryId: data['id_category'] ?? '',
+      images: imageList,
     );
   }
-
-  Future<String> getCategoryName(CategoriesRepository categoryRepo) async {
+    Future<String> getCategoryName(CategoriesRepository categoryRepo) async {
     if (categoryId.isEmpty) return 'нет категории';
     try {
       final category = await categoryRepo.getCategory(categoryId);
@@ -40,16 +38,47 @@ class News {
       return 'Неизвестная категория';
     }
   }
-
-  // Метод для получения URL изображения через ImagesRepository
-  Future<String> getImageUrl(ImagesRepository imagesRepo) async {
-    if (imageId.isEmpty) return ''; // Если нет imageId, возвращаем пустую строку
-    try {
-      final imageUrl = await imagesRepo.getImageUrl(imageId);
-      return imageUrl;
-    } catch (e) {
-      print('Ошибка при получении URL изображения: $e');
-      return '';
-    }
-  }
 }
+  // Модель для новости
+// class News {
+//   final String id;
+//   final String title;
+//   final String content;
+//   final String categoryId; // Связь с категорией
+ 
+//   final List<String> images;
+
+//   News({
+//     required this.id,
+//     required this.title,
+//     required this.content,
+//     required this.categoryId,
+//     required this.images,
+//   });
+
+//   factory News.fromFirestore(DocumentSnapshot doc) {
+//     final data = doc.data() as Map<String, dynamic>;
+//     final imageList = data['images'] is List ? (data['images'] as List).cast<String>() : <String>[];
+//     return News(
+//       id: doc.id,
+//       title: data['title'] ?? 'No Title',
+//       content: data['content'] ?? 'No Content',
+//       categoryId: data['id_category'] ?? '', // Получаем categoryId из Firestore
+//       images: imageList, // Получаем imageId из Firestore
+//     );
+//   }
+
+
+
+//   // Метод для получения URL изображения через ImagesRepository
+//   Future<String> getImageUrl(ImagesRepository imagesRepo) async {
+//     if (images.isEmpty) return ''; // Если нет imageId, возвращаем пустую строку
+//     try {
+//       final imageUrl = await imagesRepo.getImageUrl(images[0]);
+//       return imageUrl;
+//     } catch (e) {
+//       print('Ошибка при получении URL изображения: $e');
+//       return '';
+//     }
+//   }
+// }
