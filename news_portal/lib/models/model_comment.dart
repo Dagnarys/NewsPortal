e59deb91152c9564d6bbde78dc9076df6c52d295
dagnarys <super.iard@yandex.ru>
2025-05-09 14:29:12 +0300

    Create model_comment.dart

---
+++ news_portal/lib/models/model_comment.dart
@@ -0,0 +1,41 @@
+// lib/models/comment.dart
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Comment {
+  final String id;
+  final String newsId;
+  final String userId;
+  final String text;
+  final Timestamp createdAt;
+
+  Comment({
+    required this.id,
+    required this.newsId,
+    required this.userId,
+    required this.text,
+    required this.createdAt,
+  });
+
+  // Конструктор для создания объекта Comment из данных Firestore
+  factory Comment.fromFirestore(DocumentSnapshot snapshot) {
+    final data = snapshot.data() as Map<String, dynamic>;
+    return Comment(
+      id: snapshot.id,
+      newsId: data['news_id'] ?? '',
+      userId: data['user_id'] ?? '',
+      text: data['text'] ?? '',
+      createdAt: data['created_at'] ?? Timestamp.now(),
+    );
+  }
+
+  // Метод для преобразования объекта в Map (полезно при добавлении/обновлении)
+  Map<String, dynamic> toFirestore() {
+    return {
+      'news_id': newsId,
+      'user_id': userId,
+      'text': text,
+      'created_at': createdAt,
+    };
+  }
+}
