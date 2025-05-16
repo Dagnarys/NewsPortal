a8cdd05b7cb221237b58a634efd3e6cbe464b4df
dagnarys <super.iard@yandex.ru>
2025-05-16 17:40:52 +0300

    Update images.dart

--- news_portal/lib/repositories/images.dart
+++ news_portal/lib/repositories/images.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
@@ -6,8 +7,7 @@ import 'package:image_picker/image_picker.dart';
 class ImagesRepository {
   final FirebaseStorage _storage = FirebaseStorage.instance;
 
-
-    // Загрузка изображения в Firebase Storage
+  // Загрузка изображения в Firebase Storage
   Future<String> uploadImage(File imageFile, String userId) async {
     try {
       print('Создаем ссылку на Firebase Storage...');
@@ -27,35 +27,66 @@ class ImagesRepository {
       throw Exception('Ошибка загрузки изображения: $e');
     }
   }
+  
   //загрузка всех изображений в firebasestorage
   Future<List<String>> uploadImages(List<XFile> images) async {
     List<String> imageUrls = [];
 
     for (final image in images) {
       try {
-        // Генерируем уникальное имя файла
-        final fileName = '${DateTime.now().millisecondsSinceEpoch}-${image.name}';
-        final ref = _storage.ref().child('news_images/$fileName');
+        final bytes = await image.readAsBytes();
+        final fileName =
+            '${DateTime.now().millisecondsSinceEpoch}-${image.name}';
+        final ref = _storage.ref().child('news_images').child(fileName);
 
-        // Загружаем изображение в Storage
-        await ref.putFile(File(image.path));
-
-        // Получаем URL-ссылку
+        await ref.putData(bytes);
         final url = await ref.getDownloadURL();
         imageUrls.add(url);
       } catch (e) {
         print('Ошибка при загрузке изображения: $e');
-        continue;
       }
     }
 
     return imageUrls;
   }
-  Future<String> uploadImageAdd(XFile image) async {
-    final fileName = '${DateTime.now().millisecondsSinceEpoch}-${image.name}';
-    final ref = _storage.ref().child('news_images/$fileName');
 
-    await ref.putFile(File(image.path));
+  Future<String> uploadImageAdd(XFile image) async {
+    final bytes = await image.readAsBytes();
+    final fileName = DateTime.now().millisecondsSinceEpoch.toString();
+    final ref =
+        FirebaseStorage.instance.ref().child('news_images').child(fileName);
+    final uploadTask = await ref.putData(bytes);
+    final url = await uploadTask.ref.getDownloadURL();
+    return url;
+  }
+  Future<String> uploadImageMoblie({
+    required Uint8List imageBytes,
+    required String userId,
+    required String fileName,
+  }) async {
+    final ref = _storage.ref().child('news_images/$userId/$fileName');
+    await ref.putData(imageBytes);
     return await ref.getDownloadURL();
+  }
+  Future<String> uploadImageWeb({
+    required Uint8List imageBytes,
+    required String userId,
+    required String fileName,
+  }) async {
+    final storage = FirebaseStorage.instance;
+    final fileNameWithTimestamp =
+        '${DateTime.now().millisecondsSinceEpoch}_$fileName';
+    final ref = storage
+        .ref()
+        .child('profile_images')
+        .child(userId)
+        .child(fileNameWithTimestamp);
+
+    final uploadTask = await ref.putData(
+      imageBytes,
+      SettableMetadata(contentType: 'image/jpeg'),
+    );
+    
+    return await uploadTask.ref.getDownloadURL();
   }
 }
