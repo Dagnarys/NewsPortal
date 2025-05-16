9d8f75438675eea143ee5d4dd6bc8485a301fb56
dagnarys <super.iard@yandex.ru>
2025-05-16 17:42:25 +0300

    Update screen_new.dart

--- news_portal/lib/screens/screen_new.dart
+++ news_portal/lib/screens/screen_new.dart
@@ -1,61 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:news_portal/components/carousel_images_gallery.dart';
 import 'package:news_portal/components/top_bar.dart';
+import 'package:news_portal/models/model_categories.dart';
+import 'package:news_portal/models/model_news.dart';
+import 'package:news_portal/repositories/categories.dart';
 import 'package:news_portal/repositories/news.dart';
-import 'package:news_portal/screens/screen_news_main.dart';
 
 class ScreenNew extends StatefulWidget {
   final String newsId;
-  final NewsRepository repositoryNews = NewsRepository();
-  final String title;
-  final String content;
-  final List<String> imageUrls;
 
-  ScreenNew({
-    super.key,
-    required this.newsId,
-    required this.content,
-    required this.title,
-    required this.imageUrls,
-  });
+  const ScreenNew({super.key, required this.newsId});
 
   @override
   State<ScreenNew> createState() => _ScreenNewState();
 }
 
 class _ScreenNewState extends State<ScreenNew> {
+  late Future<News> _newsFuture;
   final ScrollController _scrollController = ScrollController();
-
   final TextEditingController _searchController = TextEditingController();
-
-  void _onSearchSubmitted(String value) {
-    if (value.isNotEmpty) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MainScreen(
-            searchQuery: value.toLowerCase(), // ← Передаём поисковый запрос
-          ),
-        ),
-      );
-    }
-  }
 
   @override
   void initState() {
     super.initState();
-    _scrollController.addListener(_handleScroll);
+    _newsFuture = NewsRepository().getNewsById(widget.newsId);
+    NewsRepository().incrementViewCount(widget.newsId);
   }
 
-  void _handleScroll() {
-    setState(() {});
+  void _onSearchSubmitted(String value) {
+    if (value.isNotEmpty) {
+      context.go('/',
+          extra: {'categoryId': null, 'searchQuery': value.toLowerCase()});
+    }
   }
 
-  @override
-  void dispose() {
-    _scrollController.removeListener(_handleScroll);
-    _scrollController.dispose();
-    super.dispose();
+  String _formatDate(Timestamp timestamp) {
+    final date = timestamp.toDate();
+    return '${date.day}.${date.month}.${date.year}';
   }
 
   @override
@@ -64,26 +48,44 @@ class _ScreenNewState extends State<ScreenNew> {
       backgroundColor: Colors.white,
       body: Stack(
         children: [
+          // Место для верхнего изображения
           Positioned(
             top: 0,
             left: 0,
             right: 0,
-            child: widget.imageUrls.isNotEmpty
-                ? Image.network(
-                    widget.imageUrls.first,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
+            child: FutureBuilder<News>(
+              future: _newsFuture,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Container(
                     height: 261,
-                  )
-                : Container(
+                    width: double.infinity,
                     color: Colors.grey[300],
-                    height: 261,
-                    width: double.infinity,
-                    child: Center(child: Text('Изображений нет')),
-                  ),
+                    child: Center(child: CircularProgressIndicator()),
+                  );
+                }
+
+                final news = snapshot.data!;
+                final imageUrl =
+                    news.imageUrls.isNotEmpty ? news.imageUrls.first : '';
+
+                return imageUrl.isNotEmpty
+                    ? Image.network(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                        width: double.infinity,
+                        height: 261,
+                      )
+                    : Container(
+                        height: 261,
+                        color: Colors.grey[300],
+                        child: Center(child: Text('Изображений нет')),
+                      );
+              },
+            ),
           ),
+
           CustomScrollView(
-            scrollBehavior: ScrollBehavior(),
             controller: _scrollController,
             slivers: [
               SliverAppBar(
@@ -103,104 +105,154 @@ class _ScreenNewState extends State<ScreenNew> {
                 actions: const [],
               ),
               SliverToBoxAdapter(
-                child: Container(
-                  padding: EdgeInsets.only(top: 135),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Stack(
+                child: FutureBuilder<News>(
+                  future: _newsFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(child: Text('Ошибка: ${snapshot.error}'));
+                    } else if (!snapshot.hasData || snapshot.data == null) {
+                      return Center(child: Text('Новость не найдена'));
+                    }
+
+                    final news = snapshot.data!;
+
+                    return Container(
+                      padding: EdgeInsets.only(top: 135),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Container(
-                            margin: EdgeInsets.only(top: 40),
-                            padding:
-                                EdgeInsets.only(top: 90, right: 4, left: 4),
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(16)),
-                              color: Colors.white,
-                            ),
-                            child: Text(
-                              widget.content,
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                          ),
-                          Center(
-                            child: Container(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 16, horizontal: 6),
-                              alignment: AlignmentDirectional.centerStart,
-                              width: 311,
-                              height: 109,
-                              decoration: BoxDecoration(
+                          Stack(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(top: 40),
+                                padding:
+                                    EdgeInsets.only(top: 90, right: 4, left: 4),
+                                decoration: BoxDecoration(
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(16)),
-                                  color: Color.fromRGBO(214, 212, 212, 0.979)),
-                              child: Container(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      '25 октября 2024',
-                                      style: TextStyle(
-                                          fontSize: 12,
-                                          color: Color(0xFF2E0505)),
-                                    ),
-                                    const SizedBox(height: 3),
-                                    Text(
-                                      widget.title,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Color(0xFF2E0505)),
-                                    ),
-                                    const SizedBox(height: 3),
-                                    Text(
-                                      'Автор текста: Марина Ткачева',
-                                      style: TextStyle(
-                                          fontSize: 10,
-                                          fontWeight: FontWeight.w600,
-                                          color: Color(0xFF2E0505)),
-                                    ),
-                                    const SizedBox(height: 3),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          '#категория',
-                                          style: TextStyle(
-                                              fontSize: 10,
-                                              color: Color(0xFF2E0505)),
-                                        ),
-                                        const Spacer(),
-                                        Text(
-                                          '© 1234',
-                                          style: TextStyle(
-                                              fontSize: 10,
-                                              color: Color(0xFF2E0505)),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
+                                  color: Colors.white,
+                                ),
+                                child: Text(
+                                  news.content,
+                                  style: const TextStyle(fontSize: 16),
                                 ),
                               ),
-                            ),
+                              Center(
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 16, horizontal: 6),
+                                  alignment: AlignmentDirectional.centerStart,
+                                  width: 311,
+                                  height: 109,
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(16)),
+                                    color: Color.fromRGBO(214, 212, 212, 0.979),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        _formatDate(news.publishedAt),
+                                        style: TextStyle(
+                                            fontSize: 12,
+                                            color: Color(0xFF2E0505)),
+                                      ),
+                                      SizedBox(height: 3),
+                                      Text(
+                                        news.title,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          color: Color(0xFF2E0505),
+                                        ),
+                                      ),
+                                      SizedBox(height: 3),
+                                      Text(
+                                        news.authorName,
+                                        style: TextStyle(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w600,
+                                            color: Color(0xFF2E0505)),
+                                      ),
+                                      SizedBox(height: 3),
+                                      Row(
+                                        children: [
+                                          FutureBuilder<Category?>(
+                                            future: CategoriesRepository()
+                                                .getCategory(news.categoryId),
+                                            builder: (context, snapshot) {
+                                              if (snapshot.connectionState ==
+                                                  ConnectionState.waiting) {
+                                                return Text('#загрузка');
+                                              } else if (snapshot.hasError ||
+                                                  !snapshot.hasData ||
+                                                  snapshot.data == null) {
+                                                return Text('#ошибка');
+                                              } else {
+                                                return Text(
+                                                  '#${snapshot.data!.name}',
+                                                  style: TextStyle(
+                                                      fontSize: 10,
+                                                      color: Color(0xFF2E0505)),
+                                                );
+                                              }
+                                            },
+                                          ),
+                                          Spacer(),
+                                          SvgPicture.asset(
+                                            color: Color(0xFF2E0505),
+                                            'assets/svg/eye.svg',
+                                          ),
+                                          SizedBox(
+                                            width: 2,
+                                          ),
+                                          Text(
+                                            '${news.viewCount}',
+                                            style: TextStyle(
+                                                fontSize: 10,
+                                                fontWeight: FontWeight.bold,
+                                                color: Color(0xFF2E0505)),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
-                      )
-                    ],
-                  ),
+                      ),
+                    );
+                  },
                 ),
               ),
               SliverToBoxAdapter(
-                  child: widget.imageUrls.isNotEmpty
-                      ? Column(
-                          children: [
-                            const SizedBox(height: 16),
-                            CarouselImages(imageUrls: widget.imageUrls),
-                            const SizedBox(height: 35),
-                          ],
-                        )
-                      : SizedBox())
+                child: FutureBuilder<News>(
+                  future: _newsFuture,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return SizedBox();
+                    }
+
+                    final news = snapshot.data!;
+                    return news.imageUrls.isNotEmpty
+                        ? Column(
+                            children: [
+                              SizedBox(height: 16),
+                              CarouselImages(imageUrls: news.imageUrls),
+                              SizedBox(height: 35),
+                            ],
+                          )
+                        : SizedBox();
+                  },
+                ),
+              ),
             ],
           ),
         ],
