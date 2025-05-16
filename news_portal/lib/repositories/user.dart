9bd589c444b60aca45ead26c1acb2c9fce448ea5
dagnarys <super.iard@yandex.ru>
2025-05-16 17:40:59 +0300

    Update user.dart

--- news_portal/lib/repositories/user.dart
+++ news_portal/lib/repositories/user.dart
@@ -1,13 +1,10 @@
-import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
 class UserRepository {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   // Вход пользователя по email и паролю
   Future<UserCredential> signInWithEmailAndPassword(
