0f8a0550dfa9ae9c0069e931f89e055994b8c7c2
dagnarys <super.iard@yandex.ru>
2025-05-16 17:39:20 +0300

    update fron navigator to context

--- news_portal/lib/main.dart
+++ news_portal/lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart'; // для kIsWeb
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:go_router/go_router.dart';
 import 'package:news_portal/firebase_options.dart';
-import 'package:news_portal/pages/page_news.dart';
 import 'package:news_portal/providers/category_provider.dart';
 import 'package:news_portal/providers/user_provider.dart';
 import 'package:news_portal/repositories/categories.dart';
-import 'package:news_portal/screens/screen_news_main.dart'; // MainScreen
+import 'package:news_portal/routes/mobile_routes.dart';
+import 'package:news_portal/routes/web_routes.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
   try {
     await Firebase.initializeApp(
@@ -19,45 +22,44 @@ void main() async {
   } catch (e) {
     print('Error initializing Firebase: $e');
   }
+  final userProvider = UserProvider();
+  await userProvider.loadCurrentUser();
+
+    // Подписываемся на изменения сессии
+  FirebaseAuth.instance.authStateChanges().listen((firebaseUser) async {
+    if (firebaseUser != null) {
+      await userProvider.loadCurrentUser();
+    } else {
+      userProvider.clearUser();
+    }
+  });
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CategoryProvider(CategoriesRepository())),
-        ChangeNotifierProvider(
-          create: (_) {
-            final provider = UserProvider();
-            // ❗ Переносим loadCurrentUser() позже, если он использует Firebase
-            // Например, вызываем его в initState главного экрана
-            return provider;
-          },
-        ),
+        ChangeNotifierProvider.value(value: userProvider),
       ],
       child: MyApp(),
     ),
   );
 }
-
+final _router = GoRouter(
+  
+  routes: kIsWeb || ![TargetPlatform.android, TargetPlatform.iOS].contains(defaultTargetPlatform)
+      ? webRoutes
+      : mobileRoutes,
+);
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Определяем, какую страницу показать
-    Widget homeScreen;
 
-    if (kIsWeb || ![TargetPlatform.android, TargetPlatform.iOS].contains(defaultTargetPlatform)) {
-      // Для веба и десктопа
-      homeScreen = const MainPage(); 
-    } else {
-      // Для мобильных устройств
-      homeScreen = const MainScreen();
-    }
-
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router,
       debugShowCheckedModeBanner: false,
       title: 'News Portal',
-      home: homeScreen,
     );
   }
 }
