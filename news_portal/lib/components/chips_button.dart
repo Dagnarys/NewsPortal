6dae93ffd739921074d47d700e8f2c90fe6f8fe3
dagnarys <super.iard@yandex.ru>
2025-05-16 17:36:43 +0300

    huge update

--- news_portal/lib/components/chips_button.dart
+++ news_portal/lib/components/chips_button.dart
@@ -1,18 +1,17 @@
-
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:news_portal/const/colors.dart';
 import 'package:news_portal/fonts/fonts.dart';
 import 'package:news_portal/providers/category_provider.dart';
-import 'package:news_portal/screens/screen_news_main.dart';
 import 'package:provider/provider.dart';
 
 class ChipsButton extends StatelessWidget {
-
   final String label;
   final VoidCallback? onPressed;
   final double fontSize;
 
-  const ChipsButton({super.key, required this.label, this.onPressed, required this.fontSize});
+  const ChipsButton(
+      {super.key, required this.label, this.onPressed, required this.fontSize});
 
   @override
   Widget build(BuildContext context) {
@@ -21,30 +20,23 @@ class ChipsButton extends StatelessWidget {
       margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
       padding: const EdgeInsets.fromLTRB(2, 0, 2, 0),
       child: TextButton(
-        onPressed: onPressed ??
-          () async {
-            // Получаем репозиторий категорий из провайдера
-            final categoryRepo = Provider.of<CategoryProvider>(context, listen: false).repository;
-
-            // Ищем категорию по имени
-            final category = await categoryRepo.getCategoryByName(label);
-            if (category != null) {
-              // Переходим на главный экран с ID категории
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MainScreen(selectedCategoryId: category.id),
-                ),
-                (route)=>false,
-
-              );
-            } else {
-              print('Категория с именем $label не найдена');
+        onPressed: () async {
+          final categoryRepo =
+              Provider.of<CategoryProvider>(context, listen: false).repository;
+          final category = await categoryRepo.getCategoryByName(label);
+          if (category != null) {
+            context.goNamed(
+              'mobile-news',
+              queryParameters:  {'categoryId': category.id,},
+            );
+          } else {
+            print('Категория с именем $label не найдена');
           }
         },
         style: TextButton.styleFrom(
-          minimumSize: Size(double.minPositive,double.minPositive),
-          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0), // Убираем стандартный padding
+          minimumSize: Size(double.minPositive, double.minPositive),
+          padding: const EdgeInsets.fromLTRB(
+              0, 0, 0, 0), // Убираем стандартный padding
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
             side: const BorderSide(color: AppColors.borderColor),
