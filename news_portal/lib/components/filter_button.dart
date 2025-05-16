ff18e98fecc49e7cf08e450376c33f7e31f5cd04
dagnarys <super.iard@yandex.ru>
2025-05-16 17:37:05 +0300

    Update filter_button.dart

--- news_portal/lib/components/filter_button.dart
+++ news_portal/lib/components/filter_button.dart
@@ -1,35 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:news_portal/components/news_stream.dart';
 import 'package:news_portal/const/colors.dart';
-
 class FilterButton extends StatelessWidget {
-  const FilterButton({
-    super.key,
-  });
+  final Function(SortOption, bool) onSortSelected;
+
+  const FilterButton({super.key, required this.onSortSelected});
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      left: 6, // Отступ слева
-      top: 130, // Отступ снизу
+      left: 6,
+      top: 130,
       child: Container(
         height: 28,
         width: 28,
-    
         decoration: BoxDecoration(
           gradient: AppColors.primaryGradient,
-          borderRadius: BorderRadius.all(Radius.circular(16))
+          borderRadius: BorderRadius.circular(16),
         ),
-          child: FloatingActionButton(
-            backgroundColor: Colors.transparent,
-             elevation: 0,
-            onPressed: () {
-              // Действие при нажатии на кнопку
-              //добавить фильтр
-            },
-            child: SvgPicture.asset('assets/svg/filter.svg',color: Colors.white,) // Иконка кнопки
+        child: PopupMenuButton<SortOption>(
+          itemBuilder: (context) => [
+            PopupMenuItem(
+              value: SortOption.newest,
+              child: Text('Сначала новые'),
+            ),
+            PopupMenuItem(
+              value: SortOption.oldest,
+              child: Text('Сначала старые'),
+            ),
+            PopupMenuItem(
+              value: SortOption.mostViews,
+              child: Text('Больше просмотров'),
+            ),
+            PopupMenuItem(
+              value: SortOption.leastViews,
+              child: Text('Меньше просмотров'),
+            ),
+          ],
+          onSelected: (value) {
+            bool isAscending = false;
+            if (value == SortOption.leastViews) isAscending = true;
+            onSortSelected(value, isAscending);
+          },
+          child: Container(
+            width: 28,
+            height: 28,
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/svg/filter.svg',
+              color: Colors.white,
+            ),
           ),
         ),
-      );
+      ),
+    );
   }
 }
