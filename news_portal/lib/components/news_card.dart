efcb09784606c7bb9f949a7100be1961bc13f168
dagnarys <super.iard@yandex.ru>
2025-05-16 17:38:08 +0300

    compress import data

--- news_portal/lib/components/news_card.dart
+++ news_portal/lib/components/news_card.dart
@@ -1,50 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:news_portal/fonts/fonts.dart';
-import 'package:news_portal/screens/screen_comment.dart';
-import 'package:news_portal/screens/screen_new.dart';
+import 'package:news_portal/models/model_news.dart';
+import 'package:news_portal/providers/category_provider.dart';
+import 'package:news_portal/repositories/news.dart';
+import 'package:provider/provider.dart';
 
 class NewsCard extends StatelessWidget {
-  final String title;
-  final String content;
-  final String newsId;
-  final String nameCategory;
-  final List<String> imageUrls;
+  final News news;
+  
+
   const NewsCard({
-  super.key,
-  required this.title,
-  required this.content,
-  required this.newsId,
-  required this.nameCategory,
-  required this.imageUrls,
-});
+    super.key, required this.news,
+    
+
+  });
 
   @override
   Widget build(BuildContext context) {
+     print(news.imageUrls);
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextButton(
         onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => ScreenNew(
-                        newsId: newsId,
-                        content: content,
-                        title: title,
-                        imageUrls: imageUrls,
-                      )));
+          context.push('/new/${news.id}');
         },
         child: Container(
           padding: EdgeInsets.fromLTRB(8, 55, 8, 8),
           width: 321,
           height: 205,
           decoration: BoxDecoration(
-            color: imageUrls.isEmpty ? Colors.grey[300] : null,
-            image: imageUrls.isNotEmpty
+           
+            color: news.imageUrls.isEmpty ? Colors.grey[300] : null,
+            image: news.imageUrls.isNotEmpty
                 ? DecorationImage(
-                    image: NetworkImage(imageUrls.first),
+                    image: NetworkImage(news.imageUrls.first),
                     fit: BoxFit.cover,
+                    
                   )
                 : null,
             borderRadius: BorderRadius.all(Radius.circular(16)),
@@ -52,7 +45,7 @@ class NewsCard extends StatelessWidget {
           child: Column(
             children: [
               Text(
-                title,
+                news.title,
                 style: TextStyle(
                     fontSize: 18,
                     fontFamily: AppFonts.nunitoSansFontFamily,
@@ -65,7 +58,7 @@ class NewsCard extends StatelessWidget {
               Text(
                   maxLines: 2,
                   overflow: TextOverflow.ellipsis,
-                  content,
+                  news.content,
                   style: TextStyle(
                       fontSize: 12,
                       fontFamily: AppFonts.nunitoFontFamily,
@@ -74,37 +67,58 @@ class NewsCard extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    nameCategory,
-                    style: TextStyle(
-                        fontSize: 12,
-                        fontFamily: AppFonts.nunitoFontFamily,
-                        color: Colors.white),
-                  ),
+                  FutureBuilder<String>(
+                    future: _getCategoryName(context),
+                    builder: (context, snapshot) {
+                      if (!snapshot.hasData) {
+                        return Text('#загрузка', style: TextStyle(color: Colors.white));
+                      }
+                      return Text(
+                        snapshot.data!,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Nunito',
+                          color: Colors.white,
+                        ),
+                      );
+                    }),
                   Row(
                     children: [
-                      IconButton(
+                      IconButton( padding: EdgeInsets.all(0),
+                      alignment: Alignment.centerRight,
                         onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => ScreenComment(
-                                        newsId: newsId,
-                                      )));
+                          
+                          context.pushNamed('mobile-news-comment',extra: {'title':news.title,'id':news.id});
+                          
                         },
                         icon: SvgPicture.asset(
+                          
                           'assets/svg/comment.svg',
                         ),
                       ),
-                      SizedBox(
-                        width: 2,
+                      // SizedBox(
+                      //   width: 2,
+                      // ),
+                      StreamBuilder<int>(
+                        stream: NewsRepository().getCommentCount(news.id),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasError) {
+                            return Text("Ошибка",
+                                style: TextStyle(color: Colors.red));
+                          }
+
+                          final int commentCount = snapshot.data ?? 0;
+
+                          return Text(
+                            '$commentCount',
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: AppFonts.nunitoFontFamily,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFFB3B3B3)),
+                          );
+                        },
                       ),
-                      Text('1234',
-                          style: TextStyle(
-                              fontSize: 10,
-                              fontFamily: AppFonts.nunitoFontFamily,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFFB3B3B3))),
                       SizedBox(
                         width: 4,
                       ),
@@ -114,7 +128,7 @@ class NewsCard extends StatelessWidget {
                       SizedBox(
                         width: 2,
                       ),
-                      Text('1234',
+                      Text('${news.viewCount}',
                           style: TextStyle(
                               fontSize: 10,
                               fontFamily: AppFonts.nunitoFontFamily,
@@ -129,5 +143,16 @@ class NewsCard extends StatelessWidget {
         ),
       ),
     );
+  }
+   Future<String> _getCategoryName(BuildContext context) async {
+    try {
+      final category = await Provider.of<CategoryProvider>(context, listen: false)
+          .repository
+          .getCategory(news.categoryId);
+      return '#${category?.name ?? 'без категории'}';
+    } catch (e) {
+      print('Ошибка получения категории: $e');
+      return '#ошибка';
+    }
   }
 }
