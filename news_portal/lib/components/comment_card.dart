ce0d30627b160fc1e0c81f05d59429ac5f141ebd
dagnarys <super.iard@yandex.ru>
2025-05-16 17:36:58 +0300

    Update comment_card.dart

--- news_portal/lib/components/comment_card.dart
+++ news_portal/lib/components/comment_card.dart
@@ -2,7 +2,6 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:news_portal/const/colors.dart';
 import 'package:news_portal/fonts/fonts.dart';
 import 'package:news_portal/models/model_comment.dart';
 
@@ -31,45 +30,77 @@ class CommentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<DocumentSnapshot>(
-      stream: FirebaseFirestore.instance
-          .collection('users')
-          .doc(comment.userId)
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Text("Загрузка...");
-        }
+    return Column(
+      children: [
+        StreamBuilder<DocumentSnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('users')
+              .doc(comment.userId)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Text(
+                "Загрузка...",
+                style: TextStyle(decoration: TextDecoration.none),
+              );
+            }
 
-        final userData = snapshot.data!.data() as Map<String, dynamic>;
-        return Container(
-          padding: EdgeInsets.all(10),
-          width: 344,
-          decoration: BoxDecoration(
-              gradient: AppColors.buttonGradient,
-              borderRadius: BorderRadius.all(Radius.circular(10))),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Row(
-              children: [
-                Text(
-                  "${userData['name']} ${userData['surname']}",
-                  style: TextStyle(
-                      fontFamily: AppFonts.nunitoFontFamily,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w600),
-                ),
-                Expanded(child: Container()),
-                Text(_formatTimestamp(comment.createdAt)),
-              ],
-            ),
-            Text(
-              textAlign: TextAlign.left,
-              comment.text,
-            ),
-          ]),
-        );
-      },
+            final userData = snapshot.data!.data() as Map<String, dynamic>;
+            return Container(
+              padding: EdgeInsets.all(10),
+              height: 150,
+              width: 344,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 145, 187, 240),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "${userData['name']} ${userData['surname']}",
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              color: Colors.black,
+                              fontFamily: AppFonts.nunitoFontFamily,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Expanded(child: Container()),
+                        Text(
+                          _formatTimestamp(comment.createdAt),
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: AppFonts.nunitoFontFamily,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: AppFonts.nunitoFontFamily,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600,
+                        decoration: TextDecoration.none,
+                      ),
+                      textAlign: TextAlign.left,
+                      comment.text,
+                    ),
+                  ]),
+            );
+          },
+        ),
+        
+        
+      ],
     );
   }
 }
